fdef475b50f016b559841a38e37ee548e73a4ae8
kburk1997 <hello@kburk.me>
2018-09-28 16:52:27 -0400

    fix eslint, add dummy link

--- frontend/src/components/BuildingList.vue
+++ frontend/src/components/BuildingList.vue
@@ -10,9 +10,10 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="building in buildings">
+      <tr v-for="building in buildings" :key="building.name">
         <td>
-          {{building.name}}
+          <!-- TODO: have this link to the building once skeleton individual building plan fleshed out -->
+          <a href='#'>{{building.name}}</a>
         </td>
         <td v-if="building.abbreviation">
           {{building.abbreviation}}
@@ -28,33 +29,33 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  name: "BuildingList",
-  data() {
+  name: 'BuildingList',
+  data () {
     return {
       buildings: [],
-      msg: "Welcome to Your Vue.js App"
-    };
+      msg: 'Welcome to Your Vue.js App'
+    }
   },
   methods: {
-    getBuildingList: function() {
+    getBuildingList: function () {
       axios
         .get(`/api/buildings?sort=None&filter=None&region=None`)
         .then(response => {
-          //console.log(response);
-          this.buildings = response.data;
+          // console.log(response);
+          this.buildings = response.data
         })
         .catch(error => {
-          console.error(error);
-        });
+          console.error(error)
+        })
     }
   },
-  mounted: function() {
-    this.getBuildingList();
+  mounted: function () {
+    this.getBuildingList()
   }
-};
+}
 </script>
 
 <style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
-import BuildingList from "@/components/BuildingList";
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import BuildingList from '@/components/BuildingList'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "HelloWorld",
+      path: '/',
+      name: 'HelloWorld',
       component: HelloWorld
     },
     {
-      path: "/buildings/",
-      name: "BuildingList",
+      path: '/buildings/',
+      name: 'BuildingList',
       component: BuildingList
     }
   ]
-});
+})
