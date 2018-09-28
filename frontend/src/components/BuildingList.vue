d179a9b717d2586137b371d7e9cd11d5d75d474d
kburk1997 <hello@kburk.me>
2018-09-28 15:46:09 -0400

    add building list

---
+++ frontend/src/components/BuildingList.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+  <h1>All Buildings</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Abbreviation</th>
+        <th>Region</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="building in buildings">
+        <td>
+          {{building.name}}
+        </td>
+        <td v-if="building.abbreviation">
+          {{building.abbreviation}}
+        </td>
+        <td v-if="!building.abbreviation">
+          None
+        </td>
+        <td>{{building.region}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "BuildingList",
+  data() {
+    return {
+      buildings: [],
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    getBuildingList: function() {
+      axios
+        .get(`/api/buildings?sort=None&filter=None&region=None`)
+        .then(response => {
+          //console.log(response);
+          this.buildings = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  mounted: function() {
+    this.getBuildingList();
+  }
+};
+</script>
+
+<style>
+</style>
