16bcaf9809da31c44644e44328dd09f9a3b69b1e
dong-eileen <dong.eileen2@gmail.com>
2018-10-02 14:20:38 -0400

    add schema and data for floor_plan

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,4 +14,8 @@ INSERT INTO floor (number, building_id) VALUES (1, 2);
 INSERT INTO floor (number, building_id) VALUES (2, 2);
 INSERT INTO floor (number, building_id) VALUES (3, 2);
 
-/* TODO: add mock data (Student Union) for tables: floor_plan, bathroom*/
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union1.pdf', 2, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union2.pdf', 2, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union3.pdf', 2, 3);
+
+/* TODO: add mock data (Student Union, building_id = 2) for table: bathroom*/
