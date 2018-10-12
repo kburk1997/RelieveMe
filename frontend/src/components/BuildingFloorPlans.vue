9db4bd590de5324b7d69812e44bda98a01904984
huj13 <huj13@rpi.edu>
2018-10-12 15:50:18 -0400

    changed BuildingFloorPlans now the each floor number is shown

--- frontend/src/components/BuildingFloorPlans.vue
+++ frontend/src/components/BuildingFloorPlans.vue
@@ -2,9 +2,9 @@
   <div id ="building-floor-plan">
     <h1 class="title is-2">{{$route.params.name}} Floor Plan</h1>
 
-    <div v-for="plan in plans" v-bind:key="plan.number">
+    <div v-for="plan in plans" v-bind:key="plan.floorPlanKey.floorNumber">
       <h2 class="subtitle is-3">
-        Floor {{plan.number}}  <a v-bind:href="plan.link">link </a>
+        Floor {{plan.floorPlanKey.floorNumber}}  <a v-bind:href="plan.link">link </a>
       </h2>
     </div>
   </div>
