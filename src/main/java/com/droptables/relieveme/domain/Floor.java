a2b8ec2e3511b4a26bc0f2051808a7f0d3aabed8
dong-eileen <dong.eileen2@gmail.com>
2018-10-12 14:36:31 -0400

    Finish filter by wheelchair accessibility

--- src/main/java/com/droptables/relieveme/domain/Floor.java
+++ src/main/java/com/droptables/relieveme/domain/Floor.java
@@ -3,6 +3,7 @@ package com.droptables.relieveme.domain;
 import com.droptables.relieveme.domain.key.FloorKey;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "floor")
@@ -11,11 +12,26 @@ public class Floor {
     @EmbeddedId
     private FloorKey floorKey;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumns({
+            @JoinColumn(name = "building_id", referencedColumnName = "building_id"),
+            @JoinColumn(name = "floor_number", referencedColumnName = "number")
+    })
+    private List<Bathroom> bathrooms;
+
     public FloorKey getFloorKey() {
         return floorKey;
     }
 
     public void setFloorKey(FloorKey floorKey) {
         this.floorKey = floorKey;
+    }
+
+    public List<Bathroom> getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(List<Bathroom> bathrooms) {
+        this.bathrooms = bathrooms;
     }
 }
