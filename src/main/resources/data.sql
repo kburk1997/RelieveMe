2ef684632d19f2b972cd661aff5bcd2978e9df5f
RachelTheAwesome <43351581+RachelTheAwesome@users.noreply.github.com>
2018-11-08 00:21:33 -0500

    Update src/main/resources/data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -199,63 +199,67 @@ INSERT INTO floor (number, building_id) VALUES (3, 1);
 /*-----------------*/
 /* FLOOR PLAN DATA */
 /*-----------------*/
+/* basement (floor 0) and rooftop are not added into floor plan because students cannot get there */
+
+/* Houston Field House */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20100%20Level.pdf', 1, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20200%20level.pdf', 1, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20300%20level.pdf', 1, 3);
 
 /* RPI Union */
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union1.pdf', 2, 1);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union2.pdf', 2, 2);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Union3.pdf', 2, 3);
 
-/* Amos Eaton */
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/AmosEaton100.pdf', 5, 1);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/AmosEaton200.pdf', 5, 2);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/AmosEaton300.pdf', 5, 3);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/AmosEaton400.pdf', 5, 4);
-
-/* Lally */
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/LallyHall000.pdf', 10, 0);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/LallyHall100.pdf', 10, 1);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/LallyHall200.pdf', 10, 2);
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/LallyHall300.pdf', 10, 3);
-
-/* VCC */
-INSERT INTO floor_plan (link, building_id, floor_number)
-VALUES ('http://zim2411.info/floorplans/Voorhees1.pdf', 11, 1);
-VALUES ('http://zim2411.info/floorplans/Voorhees2.pdf', 11, 2);
-VALUES ('http://zim2411.info/floorplans/Voorhees3.pdf', 11, 3);
-
-/* basement (floor 0) and rooftop are not added into floor plan because students cannot get there */
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20100%20Level.pdf', 1, 1);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20200%20level.pdf', 1, 2);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Houston%20Field%20House%20300%20level.pdf', 1, 3);
+/* Mueller */
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Mueller1000.pdf', 3, 1);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Mueller2000.pdf', 3, 2);
+
+/* Commons */
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Commons1.pdf', 4, 1);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Commons2.pdf', 4, 2);
+
+/* Amos Eaton */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/AmosEaton100.pdf', 5, 1); 
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/AmosEaton200.pdf', 5, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/AmosEaton300.pdf', 5, 3);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/AmosEaton400.pdf', 5, 4);
+
+/* Lally */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/LallyHall000.pdf', 10, 0);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/LallyHall100.pdf', 10, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/LallyHall200.pdf', 10, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/LallyHall300.pdf', 10, 3);
+
+/* Ricketts */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts100.pdf', 9, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts200.pdf', 9, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts300.pdf', 9, 3);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts400.pdf', 9, 4);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts500.pdf', 9, 5);
+
+/* Sage Lab */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab1000.pdf', 7, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab2000.pdf', 7, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab3000.pdf', 7, 3);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab4000.pdf', 7, 4);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab5000.pdf', 7, 5);
+
+/* Sage DH */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sage%20DH%20Main%20Floor.pdf', 8, 1);
+
+/* VCC */
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Voorhees1.pdf', 11, 1);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Voorhees2.pdf', 11, 2);
+INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Voorhees3.pdf', 11, 3);
+
+/* Walker */
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.1000.pdf', 6, 1);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.2000.pdf', 6, 2);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.3000.pdf', 6, 3);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.4000.pdf', 6, 4);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.5000.pdf', 6, 5);
 INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/WalkerLab.6000.pdf', 6, 6);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab1000.pdf', 7, 1);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab2000.pdf', 7, 2);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab3000.pdf', 7, 3);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab4000.pdf', 7, 4);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sagelab5000.pdf', 7, 5);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Sage%20DH%20Main%20Floor.pdf', 8, 1);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts100.pdf', 9, 1);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts200.pdf', 9, 2);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts300.pdf', 9, 3);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts400.pdf', 9, 4);
-INSERT INTO floor_plan (link, building_id, floor_number) VALUES ('http://zim2411.info/floorplans/Ricketts500.pdf', 9, 5);
 
 /*---------------*/
 /* BATHROOM DATA */
