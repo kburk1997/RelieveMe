17f024c87c5931b7ed0997bad8afccf4dbc45e82
huj13 <huj13@rpi.edu>
2018-11-28 16:22:03 -0500

    Added the rest of the building floor plans

--- src/main/resources/sqldata/floorplan.sql
+++ src/main/resources/sqldata/floorplan.sql
@@ -17,7 +17,45 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/Houston%20Field%20House%20300%20level.pdf', 1, 3);
 
-/* RPI Union */
+/* Admissions */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Admissions1.pdf', 2, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Admissions2.pdf', 2, 2);
+
+/* Alumni Sports & Recreation Center */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/AS&RC100.pdf', 3, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/AS&RC200.pdf', 3, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/AS&RC300.pdf', 3, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/AS&RC400.pdf', 3, 4);
+
+/* Mueller Center */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Mueller1000.pdf', 4, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Mueller2000.pdf', 4, 2);
+
+/* Rensselaer Union */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -31,17 +69,7 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/Union3.pdf', 6, 3);
 
-/* Mueller */
-INSERT INTO floor_plan
-    (link, building_id, floor_number)
-VALUES
-       ('http://zim2411.info/floorplans/Mueller1000.pdf', 4, 1);
-INSERT INTO floor_plan
-    (link, building_id, floor_number)
-VALUES
-       ('http://zim2411.info/floorplans/Mueller2000.pdf', 4, 2);
-
-/* Commons */
+/* Commons Dining Hall */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -50,6 +78,24 @@ INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
        ('http://zim2411.info/floorplans/Commons2.pdf', 7, 2);
+
+/* Academy Hall */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Academy%201000.pdf', 8, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Academy%202000.pdf', 8, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Academy%203000.pdf', 8, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Academy%204000.pdf', 8, 4);
 
 /* Amos Eaton */
 INSERT INTO floor_plan
@@ -69,7 +115,115 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/AmosEaton400.pdf', 9, 4);
 
-/* J-Rowl */
+/* Carnegie Building */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Carneg-1.pdf', 10, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Carneg-2.pdf', 10, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Carneg-3.pdf', 10, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Carneg-4.pdf', 10, 4);
+
+/* Center for Biotechnology and Interdisciplinary Studies */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/biotech1.pdf', 11, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/biotech2.pdf', 11, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/biotech3.pdf', 11, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/biotech4.pdf', 11, 4);
+
+/* Darrin Communications Center */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Darrin-1.pdf', 13, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Darrin-2.pdf', 13, 2);
+
+/* Folsom Library */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Folsom1.pdf', 14, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Folsom2.pdf', 14, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Folsom3.pdf', 14, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Folsom4.pdf', 14, 4);
+
+/* Greene Building */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Greene-1.pdf', 15, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Greene-2.pdf', 15, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Greene-3.pdf', 15, 3);
+
+/* Jonsson Engineering Center */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr1000.pdf', 16, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr2000.pdf', 16, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr3000.pdf', 16, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr4000.pdf', 16, 4);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr5000.pdf', 16, 5);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr6000.pdf', 16, 6);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/JonssonEngCtr7000.pdf', 16, 7);
+
+/* Jonsson-Rowland Science Center */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -87,7 +241,7 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/JonssonRowland3.pdf', 17, 3);
 
-/* Lally */
+/* Lally Hall */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -105,7 +259,73 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/LallyHall300.pdf', 18, 3);
 
-/* Ricketts */
+/* Low Center for Industrial Innovation */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII1000.pdf', 19, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII2000.pdf', 19, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII3000.pdf', 19, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII4000.pdf', 19, 4);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII5000.pdf', 19, 5);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII6000.pdf', 19, 6);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII7000.pdf', 19, 7);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII8000.pdf', 19, 8);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/LowCII9000.pdf', 19, 9);
+
+/* Pittsburgh Building */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/pittsburgh1000.pdf', 20, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/pittsburgh2000.pdf', 20, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/pittsburgh3000.pdf', 20, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/pittsburgh4000.pdf', 20, 4);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/pittsburgh5000.pdf', 20, 5);
+
+/* Playhouse */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Playhouse100.pdf', 21, 1);
+
+/* Ricketts Building */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -127,7 +347,13 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/Ricketts500.pdf', 22, 5);
 
-/* Sage Lab */
+/* Russell Sage Dining Hall */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Sage%20DH%20Main%20Floor.pdf', 23, 1);
+
+/* Russell Sage Laboratory */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -149,13 +375,29 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/Sagelab5000.pdf', 24, 5);
 
-/* Sage DH */
+/* Troy Building */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
-       ('http://zim2411.info/floorplans/Sage%20DH%20Main%20Floor.pdf', 8, 1);
+       ('http://zim2411.info/floorplans/Troy-1.pdf', 25, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Troy-2.pdf', 25, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Troy-3.pdf', 25, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Troy-4.pdf', 25, 4);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Troy-5.pdf', 25, 5);
 
-/* VCC */
+/* Voorhees Computing Center */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -169,7 +411,7 @@ INSERT INTO floor_plan
 VALUES
        ('http://zim2411.info/floorplans/Voorhees3.pdf', 26, 3);
 
-/* Walker */
+/* Walker Laboratory */
 INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
@@ -194,3 +436,53 @@ INSERT INTO floor_plan
     (link, building_id, floor_number)
 VALUES
        ('http://zim2411.info/floorplans/WalkerLab.6000.pdf', 27, 6);
+
+/* West Hall */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall100.pdf', 28, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall200.pdf', 28, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall300.pdf', 28, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall400.pdf', 28, 4);
+
+/* West Hall */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall100.pdf', 28, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall200.pdf', 28, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall300.pdf', 28, 3);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/WestHall400.pdf', 28, 4);
+
+/* 87 Gymnasium */
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Copy%20of%20Gym87_1.pdf', 28, 1);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Copy%20of%20Gym87_2.pdf', 28, 2);
+INSERT INTO floor_plan
+    (link, building_id, floor_number)
+VALUES
+       ('http://zim2411.info/floorplans/Copy%20of%20Gym87_3.pdf', 28, 3);
