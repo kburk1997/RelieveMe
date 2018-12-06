2ba3e4d4da83ac39818f73973b8ff6282b0135df
kburk1997 <hello@kburk.me>
2018-12-05 19:32:21 -0500

    get vs code to stop complaining about working tree

--- src/main/resources/sqldata/floor.sql
+++ src/main/resources/sqldata/floor.sql
@@ -6,245 +6,245 @@
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 3);
+    (1, 3);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 3);
+    (2, 3);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 3);
+    (3, 3);
 
 /* Mueller */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 4);
+    (1, 4);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 4);
+    (2, 4);
 
 /* RPI Union */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 6);
+    (1, 6);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 6);
+    (2, 6);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 6);
+    (3, 6);
 
 /* Commons */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 7);
+    (1, 7);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 7);
+    (2, 7);
 
 /* Amos Eaton */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 9);
+    (1, 9);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 9);
+    (2, 9);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 9);
+    (3, 9);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 9);
+    (4, 9);
 
 /* CBIS */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 11);
+    (1, 11);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 11);
+    (2, 11);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 11);
+    (3, 11);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 11);
+    (4, 11);
 
 /* JEC */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 16);
+    (1, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 16);
+    (2, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 16);
+    (3, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 16);
+    (4, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (5, 16);
+    (5, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (6, 16);
+    (6, 16);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (7, 16);
+    (7, 16);
 
 /*JROWL */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (0, 17);
+    (0, 17);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 17);
+    (1, 17);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 17);
+    (2, 17);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 17);
+    (3, 17);
 
 /* Lally */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (0, 18);
+    (0, 18);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 18);
+    (1, 18);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 18);
+    (2, 18);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 18);
+    (3, 18);
 
 /* Ricketts */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 22);
+    (1, 22);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 22);
+    (2, 22);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 22);
+    (3, 22);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 22);
+    (4, 22);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (5, 22);
+    (5, 22);
 
 /* Sage Lab */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 24);
+    (1, 24);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 24);
+    (2, 24);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 24);
+    (3, 24);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 24);
+    (4, 24);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (5, 24);
+    (5, 24);
 
 /* Sage DH */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 23);
+    (1, 23);
 
 /* VCC */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 26);
+    (1, 26);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 26);
+    (2, 26);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 26);
+    (3, 26);
 
 /* Walker */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (1, 27);
+    (1, 27);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (2, 27);
+    (2, 27);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 27);
+    (3, 27);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (4, 27);
+    (4, 27);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (5, 27);
+    (5, 27);
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (6, 27);
+    (6, 27);
 
 /* BELOW IS MOCK DATA */
 /* Houston Field House */
 INSERT INTO floor
     (number, building_id)
 VALUES
-       (3, 1);
+    (3, 1);
